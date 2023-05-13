36295dae0084f6e38d0709942dc4766c59f35461
vastidev <Vastiane1@gmail.com>
2023-05-12 21:40:06 -0300

    init commit

---
+++ src/main/java/dev/wakandaacadeny/produdoro_ij/ProdudoroIjApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacadeny.produdoro_ij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdudoroIjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdudoroIjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacadeny/produdoro_ij/ProdudoroIjApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacadeny.produdoro_ij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdudoroIjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
